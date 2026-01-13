fcf5e250113e0733c01aef16707f7d0db7ceeb60
Muhammad Jasim <jasimbassam0@gmail.com>
2026-01-13 22:49:55 +0530

    DOnumber

--- sale_custom/model/account_move.py
+++ sale_custom/model/account_move.py
@@ -3,6 +3,16 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    do_number = fields.Char(string="D.O. No.")
+    do_number = fields.Char(string="D.O. No.", compute="_compute_do_number", readonly=False)
     lpo_number = fields.Char(string="L.P.O. No.")
     service_date = fields.Datetime('Service Date')
+
+    def _compute_do_number(self):
+        for record in self:
+            do_number = ''
+            sale_order = record.invoice_line_ids.sale_line_ids.order_id
+            fieldservice = sale_order.fsm_order_ids
+            if fieldservice:
+                do_number = fieldservice[0].name
+            record.do_number = do_number
+
